c54d65ce55e910e2a31fb11642e2835f09c5e3d5
Vinidamiap <vinidamiaop@gmail.com>
2024-06-02 17:11:58 -0300

    Tela de Cadastro de Plano de Contas

--- myfinance-web-dotnet/Controllers/PlanoContaController.cs
+++ myfinance-web-dotnet/Controllers/PlanoContaController.cs
@@ -1,6 +1,7 @@
 
 using Microsoft.AspNetCore.Mvc;
 using myfinance_web_dotnet.Models;
+using myfinance_web_dotnet_domain.Entities;
 using myfinance_web_dotnet_service.Interfaces;
 
 namespace myfinance_web_dotnet.Controllers
@@ -33,6 +34,52 @@ namespace myfinance_web_dotnet.Controllers
             ViewBag.ListaPlanoConta = listaPlanoContasModel;
 
             return View();
+        }
+
+        [HttpGet("Cadastrar/{Id?}/")]
+        public IActionResult Cadastrar(int? Id)
+        {
+            if (Id != null)
+            {
+                var planoConta = _planoContaService.RetornarRegistro((int)Id);
+                var planoContaModel = new PlanoContaModel
+                {
+                    Id = planoConta.Id,
+                    Descricao = planoConta.Descricao,
+                    Tipo = planoConta.Tipo
+                };
+
+                return View(planoContaModel);
+            }
+
+            return View();
+        }
+
+        [HttpPost("Cadastrar/{Id?}/")]
+        public IActionResult Cadastrar(PlanoContaModel model)
+        {
+            // if (!ModelState.IsValid)
+            // {
+            //     return View();
+            // }
+            
+            var planoConta = new PlanoConta()
+            {
+                Id = model.Id,
+                Descricao = model.Descricao,
+                Tipo = model.Tipo
+            };
+
+            _planoContaService.Cadastrar(planoConta);
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet("Excluir/{Id}/")]
+        public IActionResult Excluir(int Id)
+        {
+            _planoContaService.Excluir(Id);
+            return RedirectToAction("Index");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
--- myfinance-web-dotnet/Models/PlanoContaModel.cs
+++ myfinance-web-dotnet/Models/PlanoContaModel.cs
@@ -1,9 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace myfinance_web_dotnet.Models
 {
     public class PlanoContaModel
     {
         public int? Id { get; set; }
+        [Required(ErrorMessage = "Campo obrigatório")]
+        [MinLength(3, ErrorMessage = "Mínimo de 3 caracteres")]
         public string Descricao { get; set; }
+        [Required(ErrorMessage = "Campo obrigatório")]
         public string Tipo { get; set; }
     }
 }
