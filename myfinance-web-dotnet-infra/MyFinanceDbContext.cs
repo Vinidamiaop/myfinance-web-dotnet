5bdda1e62d232dc86ef0bd899f6e855d5be547a3
Vinidamiap <vinidamiaop@gmail.com>
2024-06-03 18:06:40 -0300

    Refatoração - Utilizando variáveis de ambiente

--- myfinance-web-dotnet-infra/MyFinanceDbContext.cs
+++ myfinance-web-dotnet-infra/MyFinanceDbContext.cs
@@ -1,17 +1,25 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using myfinance_web_dotnet_domain.Entities;
 
 namespace myfinance_web_dotnet_infra
 {
     public class MyFinanceDbContext : DbContext
     {
+        private readonly IConfiguration _configuration;
         public DbSet<PlanoConta> PlanoConta { get; set; }
         public DbSet<Transacao> Transacao { get; set; }
+
+        public MyFinanceDbContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
 
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Server=localhost,1433;Database=myfinancepuc;User Id=sa;Password=1q2w3e4r@#$; Trusted_Connection=False; TrustServerCertificate=True;");
+            var connectionString = _configuration.GetConnectionString("DefaultConnection");
+            optionsBuilder.UseSqlServer(connectionString);
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
